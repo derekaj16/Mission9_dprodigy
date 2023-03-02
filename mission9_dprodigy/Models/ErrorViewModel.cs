badb5b343d75b6da4c766108c05678f7ebfa44b5
derekaj16 <dprodigy@byu.edu>
2023-03-01 18:57:02 -0700

    Add project files.

---
+++ mission9_dprodigy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mission9_dprodigy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
